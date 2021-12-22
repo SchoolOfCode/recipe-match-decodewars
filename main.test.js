147185030db11ec87d67c21fb51d9c66a01a17c7
Marco Tsang <hkmarcoot@gmail.com>
2021-12-22 16:28:16 +0000

    add test for decoder game

--- main.test.js
+++ main.test.js
@@ -1,1 +1,15 @@
 //👉 Write your tests below here:
+
+test("See if you can decode the secret message", () =>{
+    const actual = {
+        success: true,
+        payload: decoder("k co pqy kp nc, eqog cpf hkpf og.")
+    }
+
+    const expected = {
+        success: true,
+        payload: "i am now in la, come and find me."
+    }
+
+    expect(actual).toStrictEqual(expected);
+})
