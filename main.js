5e7d0817391d93feb20750ad3ac9303f32293cad
KieranSOC <kieranSOC@Kierans-MacBook-Pro.local>
2021-12-23 03:44:14 +1100

    recipe codewarriors done

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-/* 
-👉 Write your kata here!
-*/
+/* //Write a function that takes in a recipe as an obejct and returns an string with just the incredients from the recipe. Make sure the string has no spaces at the end.
+👉 Write your kata here! 
+*/ export function getIngredients(recipe) {
+  let string = "";
+  for (let i = 0; i < recipe.ingredients.length; i++) {
+    string += recipe.ingredients[i].name + " ";
+  }
+  return string.trimEnd();
+}
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+//Write a function that takes in a recipe as an obejct and returns an array with just the incredients from the recipe.
+let baconAndEggSanwich = {
+  ingredients: [
+    { name: "bacon", quantity: 3 },
+    { name: "eggs", quantity: 1 },
+    { name: "bread", quantity: 2 },
+  ],
+
+  instructions: [
+    "1. Cook bacon in frying pan",
+    "2. fry eggs in oil",
+    "3.put bread in toaster",
+  ],
+};
+
+console.log(getIngredients(baconAndEggSanwich));
--- main.test.js
+++ main.test.js
@@ -1,1 +1,27 @@
 //👉 Write your tests below here:
+import { getIngredients } from "./main.js";
+
+const recipe = {
+  ingredients: [
+    { name: "bacon", quantity: 3 },
+    { name: "eggs", quantity: 1 },
+    { name: "bread", quantity: 2 },
+  ],
+
+  instructions: [
+    "1. Cook bacon in frying pan",
+    "2. fry eggs in oil",
+    "3.put bread in toaster",
+  ],
+};
+
+test("When given a recipe, function should return a string with only ingredients from the recipe", () => {
+  //arrange
+
+  const expected = "bacon eggs bread";
+
+  //act
+  const actual = getIngredients(recipe);
+  //assert
+  expect(actual).toBe(expected);
+});
