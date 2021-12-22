1db87a3056782b209016ff0de9220d4a4d8ac6ba
Laurence279 <lnunn279@gmail.com>
2021-12-22 15:47:34 +0000

    Add test and function with solution and plan

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 /* 
 👉 Write your kata here!
+
+    Write a function that takes in two arrays and returns an array without any elements found in the second array.
+    
+    [1,2,3,4], [1,2,3] should return [4]
+    ["apples", "bananas", "grapes"], ["kiwi", "pears"] should return ["apples", "bananas", "grapes"]
+    [1,2,3], [1,2,3] should return []
+
+    Cipher/Encryption function??
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+
+//Solution
+
+export function matchArrays(arr1, arr2) {
+    // Define empty array
+    const arr3 = [];
+    // Loop through first array
+    for (let i = 0; i < arr1.length; i++) {
+        //If item is not found in 2nd array, add it to arr3
+        if (!arr2.includes(arr1[i])) {
+            arr3.push(arr1[i])
+        }
+    }
+    //Return the new array
+    return arr3;
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,17 @@
 //👉 Write your tests below here:
+import {
+    matchArrays
+} from "./main.js"
+
+
+it('should return an array if given an empty array', () => {
+    expect(matchArrays([])).toEqual([]);
+})
+
+it('should return an array that does not contain any items from the second argument', () => {
+    expect(matchArrays([1, 2, 3, 4], [1, 2, 3])).toEqual([4]);
+})
+
+it('should return ["apples", "bananas", "grapes"] when given ["apples", "bananas", "grapes"], ["kiwi", "pears"]', () => {
+    expect(matchArrays(["apples", "bananas", "grapes"], ["kiwi", "pears"])).toEqual(["apples", "bananas", "grapes"])
+})
