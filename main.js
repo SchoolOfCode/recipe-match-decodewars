fd66fe46441beb40cecae006af6c4979be7b91f0
Laurence279 <lnunn279@gmail.com>
2021-12-22 22:07:37 +0000

    Merge all kata with tests and add solutions file.

--- main.js
+++ main.js
@@ -1,60 +1,62 @@
 /* 
-👉 Write your kata here!
+👉 Write your kata here!  
+
+
+                🎄🎄🍗🎄🎄
+
+        Write a function that takes in a recipe as an object and returns a string with just the names of each ingredient from the recipe. Make sure the string has no spaces at the end.
+
+        The recipe object will always have an ingredients property which will be an array.
+
+                🎄🎄🎄🎄🎄
 */
-// You found someone leave you a secret message, 
-// go and decode the message before you get into trouble.
-// All the letters are in lowercase.
+
+export function getIngredients(recipe) {
 
 
 
-//const encodedString = "crrng"
-const encodedString = "k co pqy kp nc, eqog cpf hkpf og."
-const decodedString = "i am now in la, come and find me."
-
-//actual is crrng, expected is apple. 
-
-export function decoder(encodedString){
-    let alphabetASCLL = 0;
-    let newArray = [];
-    
-    //For every alphabet in a word
-    for(let i = 0; i < encodedString.length; i++){
-        //Convert the alphabet into ASCLL code, save it in alphabetASCLL
-        alphabetASCLL = encodedString[i].charCodeAt(0);
-
-        //Check if it is a symbol or space
-        if(alphabetASCLL >= 31 && alphabetASCLL <= 64 ||
-            alphabetASCLL >= 91 && alphabetASCLL <= 96 ||
-            alphabetASCLL >= 123 && alphabetASCLL <= 127){
-            //Convert the ASCLL code back to alphabet, and push it into array
-            newArray.push(String.fromCharCode(alphabetASCLL));
-        
-        } else {
-
-            //Move 2 alphabet ahead 
-            alphabetASCLL = alphabetASCLL - 2;
-            //a, b will become y, z 
-            if (alphabetASCLL < 97){
-                alphabetASCLL = alphabetASCLL + 26;
-            } 
-            //Just in case it moves 2 alphabet downwards
-            if (alphabetASCLL > 122){
-                alphabetASCLL = alphabetASCLL - 26;
-            }  
-            console.log(alphabetASCLL);
-            
-            //Convert the ASCLL code back to alphabet, and push it into array
-            newArray.push(String.fromCharCode(alphabetASCLL));
-        }
-    }
-    //Join the array into a string
-    let text = newArray.join("");
-
-    console.log(text);
-
-    return text;
 }
 
-//decoder(encodedString);
 
-//👉 Write the function your CodeWarriors will start with below here:
+/* 
+👉 Write your kata here!
+
+                🎄🎄📦🎄🎄
+
+        Write a function that takes in two arrays and returns an array.
+        The returned array must contain everything from the first array but not include any elements found in the second array.
+        The returned array should not be ordered differently from the first array.
+        
+        [1, "Five", 2, 8], [1,2,3,4,5,6] should return ["Five", 8]
+        ["apples", "bananas", "grapes"], ["kiwi", "pears"] should return ["apples", "bananas", "grapes"]
+        [1,2,3], [1,2,3] should return []
+
+                🎄🎄🎄🎄🎄
+
+*/
+
+export function matchArrays(arr1, arr2) {
+
+
+
+}
+
+/* 
+👉 Write your kata here!
+
+
+                🎄🎄🔑🎄🎄
+
+        You found someone leave you a secret message, 
+        go and decode the message before you get into trouble.
+        All the letters are in lowercase.
+        
+                🎄🎄🎄🎄🎄
+
+*/
+
+export function decoder(encodedString) {
+
+
+
+}
